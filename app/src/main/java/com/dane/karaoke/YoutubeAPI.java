ee866de6c2d8a0181cbead2a8043f60c09d74cb5
danesavot <dnnddane@gmail.com>
2015-12-14 16:24:08 -0600

    Configure Youtube Data API 3

---
+++ app/src/main/java/com/dane/karaoke/YoutubeAPI.java
@@ -0,0 +1,13 @@
+package com.dane.karaoke;
+
+/**
+ * Created by 984391 on 12/14/2015.
+ */
+public final class YoutubeAPI {
+
+    private YoutubeAPI() {
+
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBYH_wGyjeUY1riBNLlAqt9b9HfSM53MeU";
+}
--- app/src/main/java/com/dane/karaoke/activity/MainActivity.java
+++ app/src/main/java/com/dane/karaoke/activity/MainActivity.java
@@ -1,13 +1,15 @@
-package com.dane.karaoke;
+package com.dane.karaoke.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.dane.karaoke.R;
 
 public class MainActivity extends AppCompatActivity {
 
